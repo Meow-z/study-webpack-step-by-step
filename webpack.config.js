52bb7507c4bd2daa0bd873489a21470eefb51cca
Meow-z <372086270@qq.com>
2018-01-02 15:23:48 +0800

    在配置中指定静态文件位置

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,14 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     // 告诉服务器在哪儿找文件的
-    contentBase: './dist'
+    // devServer.publicPath 将用于确定应该从哪里提供 bundle，并且此选项优先。
+    // 默认情况下，将使用当前工作目录作为提供内容的目录，但是你可以修改为其他目录 devServer.contentBase
+    contentBase: path.join(__dirname, "dist"),
+    // 也可以从多个目录提供内容
+    // contentBase: [path.join(__dirname, "public"), path.join(__dirname, "assets")]
+    // 所有来自 dist/ 目录的文件都做 gzip 压缩
+    compress: true,
+    port: 9000
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
