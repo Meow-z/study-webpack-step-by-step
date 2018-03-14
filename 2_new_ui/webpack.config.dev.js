533fe7c3750cf431bfa269decef8e3cad3017bf6
Meow-z <372086270@qq.com>
2018-03-14 16:47:12 +0800

    新增 loader 并修改 dev server

--- 2_new_ui/webpack.config.dev.js
+++ 2_new_ui/webpack.config.dev.js
@@ -20,6 +20,49 @@ module.exports = {
     compress: true,
     port: 9000,
     // 热替换 http://www.css88.com/doc/webpack/guides/hot-module-replacement/
-    hot: true
+    inline: true
   },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',  // 在 webpack 的 module 部分的 loaders 里进行配置即可
+      query: {
+          presets: ['es2015', 'react']
+      }
+    }],
+    // webpack 根据正则表达式，来确定应该查找哪些文件，并将其提供给指定的 loader。
+    rules: [{
+        // 在这种情况下，以 .css 结尾的全部文件，都将被提供给 style-loader 和 css-loader.
+        test: /\.css$/,
+        use: [
+            'style-loader',  // http://www.css88.com/doc/webpack/loaders/style-loader/
+            'css-loader?minimize'  // minimize 告诉 css-loader 要开启 CSS 压缩。 http://www.css88.com/doc/webpack/loaders/css-loader/
+        ]
+    }, {
+        // 加载图片
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+            'file-loader'  // http://www.css88.com/doc/webpack/loaders/file-loader/
+        ]
+    }, {
+        // 加载字体
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+    }, {
+        // 加载数据
+        test: /\.(csv|tsv)$/,
+        use: [
+            'csv-loader'
+        ]
+    }, {
+        // 加载数据
+        test: /\.xml$/,
+        use: [
+            'xml-loader'
+        ]
+    }]
+  }
 }
