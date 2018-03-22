6eda29b0ef1e7e5df5dc7d07df46e865ef7576d0
Meow-z <372086270@qq.com>
2018-03-22 09:44:03 +0800

    初始化路由

--- 2_new_ui/app/main.js
+++ 2_new_ui/app/main.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import { Button } from 'antd';
 import style from './style.css';
 
@@ -8,8 +9,6 @@ import style from './style.css';
 
 class App extends Component {
   render() {
-    console.log('ok1');
-    console.log(style);
     return (<h1 className={style.font}>
             Hello,Webpack~~
             <Button type="primary">Button</Button>
@@ -17,4 +16,33 @@ class App extends Component {
   }
 }
 
-render(<App/>, window.document.getElementById('root'));
+class About extends Component {
+  render() {
+    return (<h1>About</h1>)
+  }
+}
+
+// 路由配置说明（你不用加载整个配置，
+// 只需加载一个你想要的根路由，
+// 也可以延迟加载这个配置）。
+render((
+  <BrowserRouter>
+    <div className="primary-layout">
+      <header>Our React Router 4 App</header>
+      <ul>
+        <li>
+          <Link to="/">App page</Link>
+        </li>
+        <li>
+          <Link to="/about">About page</Link>
+        </li>
+      </ul>
+      <main>
+        <Route path="/" exact component={App} />
+        <Route path="/about" component={About} />
+      </main>
+    </div>
+  </BrowserRouter>
+), window.document.getElementById('root'))
+
+// render(<App/>, window.document.getElementById('root'));
